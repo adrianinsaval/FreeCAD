7a265f8d55c700b0654019e9c356b45e7ebc0254
Adrian Insaurralde Avalos <adrianinsaval@gmail.com>
2024-05-31 15:26:02 -0400

    macOS sign and notarize script branding and fixes [skip ci]

--- src/Tools/dmg_settings.py
+++ src/Tools/dmg_settings.py
@@ -1,6 +1,6 @@
-files = [f"{containing_folder}/FreeCAD.app"]
+files = [f"{containing_folder}/Ondsel ES.app"]
 symlinks = {"Applications": "/Applications"}
-badge_icon = f"{containing_folder}/FreeCAD.app/Contents/Resources/freecad.icns"
+badge_icon = f"{containing_folder}/Ondsel ES.app/Contents/Resources/ondsel.icns"
 window_rect = ((200, 200), (600, 400))
-icon_locations = {"FreeCAD.app": (180, 150), "Applications": (420, 150)}
+icon_locations = {"Ondsel ES.app": (180, 150), "Applications": (420, 150)}
 size = "3g"
